3e3c65bfbec6d31abfef4242ebd18316954e0130
lendLand <lendLand@protonmail.com>
2021-05-08 20:00:23 +0800

    init

---
+++ SimplePriceOracle.sol
@@ -0,0 +1,49 @@
+pragma solidity ^0.5.16;
+
+import "./PriceOracle.sol";
+import "./CErc20.sol";
+
+contract SimplePriceOracle is PriceOracle {
+    mapping(address => uint) prices;
+    event PricePosted(address asset, uint previousPriceMantissa, uint requestedPriceMantissa, uint newPriceMantissa);
+    address admin;
+
+    constructor() public {
+        admin = msg.sender;
+    }
+
+    function getUnderlyingPrice(CToken cToken) public view returns (uint) {
+        if (compareStrings(cToken.symbol(), "eRNA")) {
+            return prices[address(CToken(address(cToken)))];
+        } else {
+            return prices[address(CErc20(address(cToken)).underlying())];
+        }
+    }
+
+    function setUnderlyingPrice(CToken cToken, uint underlyingPriceMantissa) public {
+        require(msg.sender == admin, "only the admin may call setUnderlyingPrice");
+        address asset = address(CErc20(address(cToken)).underlying());
+        emit PricePosted(asset, prices[asset], underlyingPriceMantissa, underlyingPriceMantissa);
+        prices[asset] = underlyingPriceMantissa;
+    }
+
+    function setDirectPrice(address asset, uint price) public {
+        require(msg.sender == admin, "only the admin may call setDirectPrice");
+        emit PricePosted(asset, prices[asset], price, price);
+        prices[asset] = price;
+    }
+
+    // v1 price oracle interface for use as backing of proxy
+    function assetPrices(address asset) external view returns (uint) {
+        return prices[asset];
+    }
+
+    function compareStrings(string memory a, string memory b) internal pure returns (bool) {
+        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
+    }
+
+    function changeAdmin(address newAdmin) public {
+        require(msg.sender == admin, "only the admin may call changeAdmin");
+        admin = newAdmin;
+    }
+}
